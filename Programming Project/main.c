315d93dc96647f7f7972e8cf8219453a44407d80
Ahmed Barakat <ahmadbarakat@Ahmads-MacBook-Pro.local>
2013-11-16 19:39:32 +0200

    Initial Commit

---
+++ Programming Project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Programming Project
+//
+//  Created by Ahmed Barakat on 11/16/13.
+//  Copyright (c) 2013 Ahmed Barakat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
